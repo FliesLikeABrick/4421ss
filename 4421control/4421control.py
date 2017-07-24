53309c1dce253e3789cd55cf117e422c263b77dc
Ryan Rawdon <rrawdon@conversantmedia.com>
2017-07-24 22:39:41 +0000

    [4421control] first pass at config-ization of the tool's parameters, including yaml that defines options' defaults

--- 4421control/4421control.py
+++ 4421control/4421control.py
@@ -5,11 +5,7 @@ import sys,os
 import glob
 import traceback
 import syslog
-
-# directory containing operation scripts
-# of name format target-state.py
-SCRIPTDIR = "bin"+os.sep
-syslog.openlog(ident='4421control/app.py')
+import yaml
 
 app = Flask(__name__)
 
@@ -39,16 +35,16 @@ def control(target,state):
         return render_template("control.html",commands = [ command ]), 400
 
     try:
-        command['output'] = subprocess.check_output(SCRIPTDIR+command['script'],shell=True).decode("utf-8")
+        command['output'] = subprocess.check_output(_CFG['scripts']['directory']+command['script'],shell=True).decode("utf-8")
     except Exception as e:
         command['output'] = 'Exception encountered during script execution: ' + str(e)
     # Fire off syslog for this state change
-    syslog.syslog(command.__str__())
+    log(command.__str__())
     return render_template("control.html",commands = [ command ])
 
 # Ideally this would be cached, but currently doesn't hurt performance (at least with low number of scripts)
 def getScripts():
-    scripts = [scriptname.split(os.sep)[-1] for scriptname in glob.glob(SCRIPTDIR+'*-*.py') ]
+    scripts = [scriptname.split(os.sep)[-1] for scriptname in glob.glob(_CFG['scripts']['directory']+'*-*.py') ]
     scriptDirectory = {}
     for script in scripts:
         try:
@@ -64,6 +60,55 @@ def getScripts():
             traceback.print_exc(file=sys.stderr)
             continue
     return scriptDirectory
+def log(entry):
+    """ Handle logging to syslog and files """
+    if _CFG['syslog']['enable']:
+        syslog.syslog(entry)
+def loadConfig(ucf,dcf):
+    # open defaults configuration file
+    try:
+        with open(dcf) as yf:
+            cfg = yaml.load(yf.read())
+    except Exception as e:
+        sys.stderr.write("Failed to open configuration file %s\n%s\n" % (dcf,str(e)))
+        sys.exit(1)
+    # open user configuration file
+    try:
+        with open(ucf) as yf:
+            userCfg = yaml.load(yf.read())
+    except Exception as e:
+        sys.stderr.write("Failed to open configuration file %s\n%s\n" % (ucf,str(e)))
+        sys.exit(1)
+
+    # update the default's config sections
+    # Alternatively this could recursively update the dcfg dict with cfg
+    # in the meantime, however, this requires a line for each section of the config
+    # and potentially more depth/care if layers of the config should not be wholesale replaced
+    for section in userCfg.keys():
+        if section in cfg:
+            cfg[section].update(userCfg[section])
+        else:
+            cfg[section] = userCfg[section]
+
+    return cfg
+def initialize():
+    # directory containing state and status scripts
+    # os.path.join is used to add the trailing OS path separator if it is not already present in the config
+    _CFG['scripts']['directory'] = os.path.join(os.path.expanduser(_CFG['scripts']['directory']), '')
+    if not os.path.isdir(_CFG['scripts']['directory']):
+        sys.stderr.write("Scripts directory %s does not exist or is not a directory\n" % _CFG['scripts']['directory'])
+        sys.exit(1)
+
+    # set up syslog, if enabled
+    if _CFG['syslog']['enable']:
+        if 'ident' in _CFG['syslog']:
+            syslog.openlog(ident=_CFG['syslog']['ident'])
+
 if __name__ == "__main__":
+    # ideally take this filename as a default, and override with a command line option
+    cfgFile=os.path.dirname(os.path.realpath(__file__)) + os.sep + "4421control.cfg.yaml"
+    defaultCfgFile=os.path.dirname(os.path.realpath(__file__)) + os.sep + "4421control.cfg.defaults.yaml"
+    _CFG = loadConfig(cfgFile,defaultCfgFile)
+    initialize()
     app.run(host='::',debug=True)
 
