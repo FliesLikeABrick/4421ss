87f7595b318e5a31e402c7ba7662704f89f5520f
Ryan Rawdon <ryan@u13.net>
2017-07-25 08:53:52 -0500

    Added command line options for configuration behavior instead of hardcoding config paths.

--- 4421control/4421control.py
+++ 4421control/4421control.py
@@ -1,4 +1,12 @@
 #! /usr/bin/env python3
+"""
+API and UI application for 4421control.
+
+Notes:
+_CFG is used globally to store the default and user configurations, as well as
+"Derived" configurations (sanitized paths+inputs, compiled expressions, etc)
+
+"""
 from flask import Flask,render_template
 import subprocess
 import sys,os
@@ -7,13 +15,13 @@ import traceback
 import syslog
 import yaml
 import re
+from argparse import ArgumentParser
 
 app = Flask(__name__)
 
 @app.route("/")
 def menu():
-
-    return render_template("index.html",targets=getScripts())
+    return render_template("index.html",targets=getScripts(),cfg=_CFG)
 
 # Update target to desired state
 # Returns 404 if the target does not exist
@@ -28,12 +36,12 @@ def control(target,state):
         # target does not exist
         command['output'] = 'Exception encountered during script execution: target %s does not exist' % target
         # (don't syslog invalid requests)
-        return render_template("control.html",commands = [ command ]), 404
+        return render_template("control.html",commands = [ command ],cfg=_CFG), 404
     if not state in scripts[target]['states']:
         # target does not exist
         command['output'] = 'Exception encountered during script execution: target %s does not offer state %s' % (target,state) 
         # (don't syslog invalid requests)
-        return render_template("control.html",commands = [ command ]), 400
+        return render_template("control.html",commands = [ command ],cfg=_CFG), 400
 
     try:
         command['output'] = subprocess.check_output(_CFG['scripts']['directory']+command['script'],shell=True).decode("utf-8")
@@ -41,7 +49,7 @@ def control(target,state):
         command['output'] = 'Exception encountered during script execution: ' + str(e)
     # Fire off syslog for this state change
     log(command.__str__())
-    return render_template("control.html",commands = [ command ])
+    return render_template("control.html",commands = [ command ],cfg=_CFG)
 
 # Ideally this would be cached, but currently doesn't hurt performance (at least with low number of scripts)
 def getScripts():
@@ -66,14 +74,23 @@ def loadConfig(ucf,dcf):
         with open(dcf) as yf:
             cfg = yaml.load(yf.read())
     except Exception as e:
-        sys.stderr.write("Failed to open configuration file %s\n%s\n" % (dcf,str(e)))
+        sys.stderr.write("Failed to load defaults configuration file %s\n%s\n" % (dcf,str(e)))
         sys.exit(1)
+
+    # If no user config file is specified, return the defaults.
+    if ucf is None:
+        return cfg
     # open user configuration file
     try:
         with open(ucf) as yf:
             userCfg = yaml.load(yf.read())
-    except Exception as e:
+    except FileNotFoundError as e:
         sys.stderr.write("Failed to open configuration file %s\n%s\n" % (ucf,str(e)))
+        sys.stderr.write("Copy 4421control.cfg.example.yaml to 4421control.cfg.yaml or\n")
+        sys.stderr.write("use --defaults to run without a configuration file, which will all default values (not recommended).\n")
+        sys.exit(1)
+    except Exception as e:
+        sys.stderr.write("Failed to load configuration file %s\n%s\n" % (ucf,str(e)))
         sys.exit(1)
 
     # update the default's config sections
@@ -111,9 +128,17 @@ def initialize():
 
 if __name__ == "__main__":
     # ideally take this filename as a default, and override with a command line option
-    cfgFile=os.path.dirname(os.path.realpath(__file__)) + os.sep + "4421control.cfg.yaml"
-    defaultCfgFile=os.path.dirname(os.path.realpath(__file__)) + os.sep + "4421control.cfg.defaults.yaml"
-    _CFG = loadConfig(cfgFile,defaultCfgFile)
+    parser = ArgumentParser(description='UI and API server for 4421control: https://github.com/FliesLikeABrick/4421ss/')
+    parser.add_argument('--defaults', action='store_true')
+    parser.add_argument('--config', default=os.path.dirname(os.path.realpath(__file__)) + os.sep + "4421control.cfg.yaml")
+    parser.add_argument('--defaultconfig', default=os.path.dirname(os.path.realpath(__file__)) + os.sep + "4421control.cfg.defaults.yaml")
+    args = parser.parse_args()
+    print(args.defaults)
+    if args.defaults:
+        sys.stderr.write("--defaults specified, running with default configuration.")
+        cfgFile = None
+
+    _CFG = loadConfig(args.config,args.defaultconfig)
     initialize()
     # exit status codes:
     # 1 - config issue
